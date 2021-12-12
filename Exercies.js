e2e8a064f0464ade7cf0829c4b3d5f930bcb6df1
Shoham <shoam4183@gmail.com>
2021-12-12 16:01:12 +0200

    Exercises+Daily

---
+++ Exercies.js
@@ -0,0 +1,64 @@
+// Exercise1: Your Favorite Food
+let favoriteFood = "Shawarma"
+let favoriteMeal = "Dinner"
+console.log(`I eat ${favoriteFood} at every ${favoriteMeal}`);
+
+// Exercise2: Series
+//Part1
+let watchedSeries = ["black mirror", "money heist", "the big bang theory"];
+let watchSeriesLength = 3;
+let myWatchSeries = ["black mirror", "money heist", "the big bang theory"];
+let lengthArray = watchSeriesLength;
+console.log(`I watched ${watchSeriesLength} series ${myWatchSeries}`);
+
+//Part2
+watchedSeries[2] = "friends";
+watchedSeries.push("supernatural");
+watchedSeries.unshift("Grim");
+watchedSeries.splice(1,1);
+console.log(watchedSeries[1].charAt(2));
+console.log(watchedSeries);
+
+//Exercise3 : The Temperature
+let temperature = 60;
+let celsiusToFahrenheit = temperature * 9/5 + 32;
+console.log(celsiusToFahrenheit);
+
+//Exercise4
+1. 55
+2. 23
+3. no value
+4. 3+4=7 and then it place it next to the 5 -- Answer:75 --
+
+//Exercise5- Guess The Answer
+1. Number
+2. Number
+3. Number
+4. String
+5. Boolean
+6. True
+7. Hamburgers
+8. Hamburger
+9. 13
+10. -2
+11. Johnny5
+12. NaN
+13. False
+14. True
+15. False
+
+//Exercise6
+1. 534
+2. 1
+3. Prediction - 2 / Actual- 0
+4. Prediction - 0.5 / Actual- 5
+5. Javascript
+6. Prediction- had no idea / Actual - "
+7. Prediction- 0 / Actual - "0"
+8. 2
+9. 1
+10. 1
+11. -1
+12. False
+13. -1
+14. NaN
